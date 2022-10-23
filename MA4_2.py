28f17ee6a3092d5ce353fffd9c40c585a4f4679f
albinsand <hansalbinsand@gmail.com>
2022-10-23 14:16:16 +0200

    hejhej

--- MA4_2.py
+++ MA4_2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3.9
 
+from cProfile import label
 from time import perf_counter as pc
 from person import Person
 from numba import njit
+import matplotlib.pyplot as plt
 
 
 @njit
@@ -24,57 +26,41 @@ def main():
 	f.set(9)
 	print(f.get())
 
-	print(f.fib(10))
-	print(fib_numb(10))
-	print(fib_py(10))
+	nn = range(30,46)
+	tid_py = []
+	tid_numb = []
+	tid_fib = []
+
+	for n in nn:
+		start = pc()
+		print(f.fib(n))
+		end = pc()
+		t = end - start
+		tid_fib.append(t)
+
+	
+		start = pc()
+		print(fib_numb(n))
+		end = pc()
+		tt = end - start
+		tid_numb.append(tt)
+
+	
+		start = pc()
+		print(fib_py(n))
+		end = pc()
+		ttt = end - start
+		tid_py.append(ttt)
+
+
+	plt.plot(nn,tid_fib,label ="fib - C++")
+	plt.plot(nn,tid_numb,label ="fib - numb")
+	plt.plot(nn,tid_py,label ="fib - python")
+
+
+	#plt.savefig(f'piaprox-{n[i]}.png')
 
 if __name__ == '__main__':
 	main()
-
-
-'''	
-nn = 47
-print('N: ', nn)
-start = pc()
-f = Person(nn)
-f.fib()
-end = pc()
-print('fibc: ', end - start)
-
-start = pc()
-fib_numb(nn)
-end = pc()
-print('fib_numb: ', end - start)
-
-x = range(30, 46)
-y_py = []
-y_numba = []
-y_c = []
-
-
-for n in x:
-	print('N: ', n)
-	start = pc()
-	fib_py(n)
-	end = pc()	
-	y_py.append(end-start)
-	print('fib_py: ', end-start)
-	start = pc()
-	fib_numb(n)
-	end = pc()
-	y_numba.append(end - start)
-	print('fib_numba: ', end-start)
-	start = pc()
-	f = Person(n)
-	f.fib()
-	end = pc()
-	y_c.append(end - start)
-	print('fibc: ', end-start)
-'''
-
-#plt.plot(x, y_py, label='Python')
-#plt.plot(x, y_numba, label='Numba')
-#plt.plot(x, y_c, label='C++')
-#plt.savefig('plot_MA42.png')
 
 
